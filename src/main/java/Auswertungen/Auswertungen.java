77ad9d793de32d1b37ce77db256e7f6d624b04e7
Bruno Thurnherr <bruno.thurnherr@it-wissen.ch>
2021-01-26 21:46:09 +0100

    26.01.2021 Bruno Thurnherr

--- src/main/java/Auswertungen/Auswertungen.java
+++ src/main/java/Auswertungen/Auswertungen.java
@@ -3,14 +3,14 @@ package Auswertungen;
 import Datenbank.AuswertungenDatenbank;
 import Utilities.BefehlsZeilenSchnittstelle;
 
-public class Auswertungen{
+public class Auswertungen {
 
-    private String[] unterMenue = {"Auswertungen", "1.  Mitarbeiter","2.  Kurse Anzeigen","3.  Budget","4.  Ausgaben", "99. Hauptmenue"};
-    private String[] unterMenueMitarbeiter = {"Mitarbeiter", "1.  Aktive Mitarbeiter", "2.  Auswertung Anzahl Mitarbeiter pro Kostenstelle", "3.  Abgelaufene Zertifikate","99. Zurueck"};
-    private String[] unterMenueKurse = {"Kurse", "1.  Kurse pro Anbieter", "2.  Kurse nach Land", "99.  Zurueck"};
+    private String[] unterMenue = {"Auswertungen", "1.  Mitarbeiter", "2.  Kurse Anzeigen", "3.  Budget", "4.  Ausgaben", "99. Hauptmenue"};
+    private String[] unterMenueMitarbeiter = {"Mitarbeiter", "1.  Aktive Mitarbeiter", "2.  Auswertung Anzahl Mitarbeiter pro Kostenstelle", "3.  Abgelaufene Zertifikate", "99. Zurueck"};
+    private String[] unterMenueKurse = {"Kurse", "1.  Kurse pro Mitarbeiter", "2.  dddd", "99.  Zurueck"};
     private String[] unterMenueBudget = {"Budget", "1.  Budgetuebersicht pro Kostenstelle", "2.  Budget Ist/Soll Vergleich", "99.  Zurueck"};
-    private String[] unterMenueAusgaben = {"Ausgaben", "1.  Kurskosten pro Anbieter und Jahr", "2.  Kurskosten pro Jahr / Anbieter und Kostenstelle ", "99.  Zurueck"};
-    private final String[] viewBefehle = {"view_Bruno_TEST","view_kurse_auswertung"};
+    private String[] unterMenueAusgaben = {"Ausgaben", "1.  Kurskosten pro Anbieter / Jahr / Waehrung alle Mitarbeiter", "2.  Kurskosten pro Jahr / Anbieter / Kostenstelle", "99.  Zurueck"};
+    private final String[] viewBefehle = {"view_Bruno_TEST", "view_kurse_auswertung"};
 
     public Auswertungen() {
         untermenuAnzeigen();
@@ -18,15 +18,16 @@ public class Auswertungen{
 
     public Auswertungen(String kontext) {
     }
+
     //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
        /*
     Methode zum Anzeigen des Untermenüs
      */
-    private void untermenuAnzeigen(){
+    private void untermenuAnzeigen() {
         boolean gueltigeEingabe = false;
 
-        do{
-            switch (BefehlsZeilenSchnittstelle.unterMenue(unterMenue)){
+        do {
+            switch (BefehlsZeilenSchnittstelle.unterMenue(unterMenue)) {
                 case 1:
                     unterMenuMitarbeiterAnzeigen();
                     break;
@@ -47,21 +48,21 @@ public class Auswertungen{
                     System.out.println("Falsche Eingabe");
                     gueltigeEingabe = false;
             }
-        }while(!gueltigeEingabe);
+        } while (!gueltigeEingabe);
     }
 
     //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
        /*
     Methode zum Anzeigen des Untermenüs für die Auswertungen der Mitarbeiter
      */
-    private void unterMenuMitarbeiterAnzeigen(){
+    private void unterMenuMitarbeiterAnzeigen() {
         boolean gueltigeEingabe = false;
         MitarbeiterAuswertung mitarbeiterAuswertung = new MitarbeiterAuswertung();
 
-        do{
-            switch (BefehlsZeilenSchnittstelle.unterMenue(unterMenueMitarbeiter)){
+        do {
+            switch (BefehlsZeilenSchnittstelle.unterMenue(unterMenueMitarbeiter)) {
                 case 1:
-                   mitarbeiterAuswertung.ausgabeListe(new AuswertungenDatenbank().auswertungAusDbLesen(viewBefehle[0]));
+                    mitarbeiterAuswertung.ausgabeListe(new AuswertungenDatenbank().auswertungAusDbLesen(viewBefehle[0]));
                     break;
                 case 2:
                     System.out.println(unterMenueMitarbeiter[1]);
@@ -77,17 +78,18 @@ public class Auswertungen{
                     System.out.println("Falsche Eingabe");
                     gueltigeEingabe = false;
             }
-        }while(!gueltigeEingabe);
+        } while (!gueltigeEingabe);
     }
+
     //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
        /*
     Methode zum Anzeigen des Untermenüs für die Auswertungen der Kurse
      */
-    private void unterMenueKurseAnzeigen(){
+    private void unterMenueKurseAnzeigen() {
         boolean gueltigeEingabe = false;
 
-        do{
-            switch (BefehlsZeilenSchnittstelle.unterMenue(unterMenueKurse)){
+        do {
+            switch (BefehlsZeilenSchnittstelle.unterMenue(unterMenueKurse)) {
                 case 1:
                     System.out.println(unterMenueKurse[0]);
                     break;
@@ -102,18 +104,18 @@ public class Auswertungen{
                     System.out.println("Falsche Eingabe");
                     gueltigeEingabe = false;
             }
-        }while(!gueltigeEingabe);
+        } while (!gueltigeEingabe);
     }
 
-//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     /*
  Methode zum Anzeigen des Untermenüs für die Auswertungen des Budget
   */
-    private void unterMenueBudgetAnzeigen(){
+    private void unterMenueBudgetAnzeigen() {
         boolean gueltigeEingabe = false;
 
-        do{
-            switch (BefehlsZeilenSchnittstelle.unterMenue(unterMenueBudget)){
+        do {
+            switch (BefehlsZeilenSchnittstelle.unterMenue(unterMenueBudget)) {
                 case 1:
                     System.out.println(unterMenueBudget[0]);
                     break;
@@ -128,7 +130,7 @@ public class Auswertungen{
                     System.out.println("Falsche Eingabe");
                     gueltigeEingabe = false;
             }
-        }while(!gueltigeEingabe);
+        } while (!gueltigeEingabe);
     }
 
 
@@ -136,21 +138,25 @@ public class Auswertungen{
     /*
  Methode zum Anzeigen des Untermenüs für die Auswertungen der Ausgaben
   */
-    private void unterMenueAusgabenAnzeigen(){
+    private void unterMenueAusgabenAnzeigen() {
         boolean gueltigeEingabe = false;
 
-        do{
-            switch (BefehlsZeilenSchnittstelle.unterMenue(unterMenueAusgaben)){
+        do {
+            switch (BefehlsZeilenSchnittstelle.unterMenue(unterMenueAusgaben)) {
                 case 1:
                     System.out.println(unterMenueAusgaben[0]);
                     break;
                 case 2:
                     System.out.println(unterMenueAusgaben[1]);
                     break;
+                case 99:
+                    System.out.println(unterMenueMitarbeiter[3]);
+                    gueltigeEingabe = true;
+                    break;
                 default:
                     System.out.println("Falsche Eingabe");
                     gueltigeEingabe = false;
             }
-        }while(!gueltigeEingabe);
+        } while (!gueltigeEingabe);
     }
 }
---
+++ src/main/java/Auswertungen/KurseAuswertung.java
@@ -0,0 +1,13 @@
+package Auswertungen;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class KurseAuswertung {
+}
+
+
+
