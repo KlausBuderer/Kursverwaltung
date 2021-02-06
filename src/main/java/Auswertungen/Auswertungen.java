19fe9de909a376cb5a7f8888c5753ae93200caf1
Bruno Thurnherr <bruno.thurnherr@it-wissen.ch>
2021-02-06 08:31:33 +0100

    Auswertung Menü Struktur Anpassung

--- src/main/java/Auswertungen/Auswertungen.java
+++ src/main/java/Auswertungen/Auswertungen.java
@@ -6,7 +6,7 @@ import Utilities.BefehlsZeilenSchnittstelle;
 public class Auswertungen {
 
     private String[] unterMenue = {"Auswertungen", "1.  Mitarbeiter", "2.  Kurse Anzeigen", "3.  Budget", "4.  Ausgaben", "99. Hauptmenue"};
-    private String[] unterMenueMitarbeiter = {"Mitarbeiter", "1.  Weiterbildung aller Mitarbeiter", "2.  Auswertung Anzahl Mitarbeiter pro Kostenstelle", "3.  Abgelaufene Zertifikate", "99. Zurueck"};
+    private String[] unterMenueMitarbeiter = {"Mitarbeiter", "1.  Weiterbildung aller Mitarbeiter", "2.  Auswertung Anzahl Mitarbeiter pro Kostenstelle", "99. Zurueck"};
     private String[] unterMenueKurse = {"Kurse", "1.  Kurse pro Mitarbeiter", "2.  dddd", "99.  Zurueck"};
     private String[] unterMenueBudget = {"Budget", "1.  Budgetuebersicht pro Kostenstelle", "2.  Budget Ist/Soll Vergleich", "99.  Zurueck"};
     private String[] unterMenueAusgaben = {"Ausgaben", "1.  Kurskosten pro Anbieter / Jahr / Waehrung alle Mitarbeiter", "2.  Kurskosten pro Jahr / Anbieter / Kostenstelle", "99.  Zurueck"};
@@ -66,9 +66,6 @@ public class Auswertungen {
                     break;
                 case 2:
                     System.out.println(unterMenueMitarbeiter[1]);
-                    break;
-                case 3:
-                    System.out.println(unterMenueMitarbeiter[2]);
                     break;
                 case 99:
                     System.out.println(unterMenueMitarbeiter[3]);
