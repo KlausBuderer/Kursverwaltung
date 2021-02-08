f4244c5e8ec0e3ab61a801c42ada1307bb358c80
KlausBuderer <67792948+KlausBuderer@users.noreply.github.com>
2021-02-08 22:56:52 +0100

    08.02.2021 Klaus Buderer
    Kurs Auswertung gedebugt

--- src/main/java/Datenbank/Datenbank.java
+++ src/main/java/Datenbank/Datenbank.java
@@ -393,7 +393,11 @@ public class Datenbank {
                     dbInhalt = statement.executeQuery();
                     rueckgabeList = new AuswertungenDatenbank().ausfuehrenZertifikateAlleMitarbeiter(dbInhalt);
                     break;
+                case AUSWERTUNG_ALLE_KURSE:
 
+                    dbInhalt = statement.executeQuery();
+                    rueckgabeList = new AuswertungenDatenbank().ausfuehrenKurseAlleMitarbeiter(dbInhalt);
+                    break;
             }
 
         } catch (SQLException | ClassNotFoundException sqlException) {
